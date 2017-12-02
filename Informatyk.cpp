172412d468d4156f9fb1010af12f6fc599447f34
Mietek007 <34188498+Mietek007@users.noreply.github.com>
2017-12-02 15:52:33 +0100

    0.3
    dodatek dalej

--- Informatyk.cpp
+++ Informatyk.cpp
@@ -10,7 +10,7 @@ using namespace std;
 Informatyk::Informatyk(string imie, string nazwisko, int ID)
 	:Pracownik(imie,nazwisko,ID)
 {
-	
+	cout << "cos" << endl;
 	stawka = przelicznik*stawka;
 	cout << "-> Tworze obiekt(Informatyk)" << licznik << endl;
 }
